090c3096670ec2373a6cfc2a2f4710c76a9a8e13
vrushali <vrushali.r@spenmo.com>
2023-09-08 20:05:06 +0530

    [vrushali] initial setup of account service

--- src/main/java/com/intuit/craftDemo/AccountService.java
+++ src/main/java/com/intuit/craftDemo/AccountService.java
@@ -1,2 +1,12 @@
-package com.intuit.craftDemo;public class AccountService {
+package com.intuit.craftDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountService {
+    public static void main(String[] args) {
+        System.out.println("Hello account service");
+        SpringApplication.run(AccountService.class, args);
+    }
 }
---
+++ src/main/java/com/intuit/craftDemo/ProblemStatement.md
@@ -0,0 +1,7 @@
+# Problem Statement 
+
+• Please implement a simple account/authentication service. At a minimum, your application should be able to
+- create a user account,
+- look-up a user, and authenticate a user via password. 
+- Please demo sample http requests/responses for each of the APIs that you create.
+ 
